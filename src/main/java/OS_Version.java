af1fe6795a68f60d8d5c08dffbd22c11f7b3ac09
stalex90 <stalex20@gmail.com>
2016-11-17 15:16:42 +0500

    Merge branch 'master' of https://github.com/stalex90/Tests
    # Conflicts:
    #	src/test/java/RegistrationSellerTest.java

--- src/main/java/OS_Version.java
+++ src/main/java/OS_Version.java
@@ -7,9 +7,11 @@ public class OS_Version {
     public void SetChromeProperty(){
         if(isWindows()){
             System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+            System.out.println(getOSVerion());
         }
         if (isUnix()){
             System.setProperty("webdriver.chrome.driver","/usr/lib/chromium/chromedriver");
+            System.out.println(getOSVerion());
         }
     }
 
@@ -27,5 +29,10 @@ public class OS_Version {
         //linux or unix
         return (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0);
 
+    }
+
+    public static String getOSVerion() {
+        String os = System.getProperty("os.version");
+        return os;
     }
 }
