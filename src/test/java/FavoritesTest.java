edd9d9660c4545279ab2ebe8414500d5c4636a97
Администратор <stalex20@gmail.com>
2017-02-02 16:10:03 +0500

    30.01.2017

--- src/test/java/FavoritesTest.java
+++ src/test/java/FavoritesTest.java
@@ -1,10 +1,16 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -144,14 +150,21 @@ public class FavoritesTest {
         objFavorites.ClickFavorites();
         objFavorites.ClickSelectAllCheckbox();
         objFavorites.ClickDeleteFromFavor();
+        objFavorites.ClickYes();
         Assert.assertEquals(objFavorites.GetSuccessMsg(),objFavorites.SuccessDeleteMsg);
 
     }
 
 
     @AfterMethod
-    public static void closeBrowser() throws InterruptedException {
-        Thread.sleep(3000);
+    public void closebrowser(ITestResult testResult) throws IOException {
+        if (objOS_Version.isUnix()) {
+            if (testResult.getStatus() == ITestResult.FAILURE) {
+                File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                String path = "/var/lib/jenkins/workspace/Тест личный кабинет (Избранное)/screenshots/" + testResult.getName() + ".jpg";
+                FileUtils.copyFile(scrFile, new File(path));
+            }
+        }
         driver.quit();
     }
 }
