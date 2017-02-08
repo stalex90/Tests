c045c3060087aca6889c7ce0447ad0e8065677a5
Администратор <stalex20@gmail.com>
2017-02-08 14:44:16 +0500

    30.01.2017

--- src/test/java/UrlTest.java
+++ src/test/java/UrlTest.java
@@ -51,12 +51,121 @@ public class UrlTest {
         }
 
         @Test(description = "Проверка появление количества на иконке корзины")
-        public void CheckDisplayCount(){
+        public void CheckDisplayCount1(){
             objHomePage = new HomePage(driver);
             objCatalog = new Catalog(driver);
 
             System.out.println(driver.getCurrentUrl());
         }
+
+    @Test(description = "Проверка появление количества на иконке корзины")
+    public void CheckDisplayCount(){
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(10);
+        Assert.assertTrue(driver.findElement(objHomePage.CartCount).isDisplayed());
+    }
+
+    @Test(description = "Добавление в корзину 8 элементов и Сравнить количество товаров в корзине и количество на иконке корзины")
+    public void CheckCartCountandCountItem()  {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.AddFirst8Item();
+        objHomePage.ClickCartIcon();
+        Assert.assertEquals(objHomePage.GetCartCount(),objHomePage.GetItemsCount());
+    }
+
+
+    @Test(description = "Проверить удаление товара")
+    public void CheckDelete()  {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.AddFirst8Item();
+        objHomePage.ClickCartIcon();
+        objHomePage.DeleteItem(3);
+        Assert.assertEquals(objHomePage.GetCartCount(),7);
+    }
+
+   /* @Test (description = "Добавить количество для товара")
+    public void CheckAddItemCount(){
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(7);
+        objHomePage.ClickCartIcon();
+        objHomePage.ItemCountAdd(1);
+        Assert.assertEquals(objHomePage.GetItemCount(1),2);
+    }*/
+
+    @Test (description = "Убавить количество для товара")
+    public void CheckRemoveItemCount(){
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(7);
+        objHomePage.ClickCartIcon();
+        objHomePage.ItemCountRemove(1);
+        Assert.assertEquals(objHomePage.GetItemCount(1),0);
+    }
+
+    @Test(description = "Сложить сумму всех товаров и проверить общую сумму")
+    public void CheckFinalCost()  {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.AddFirst8Item();
+        objHomePage.ClickCartIcon();
+        Assert.assertEquals(objHomePage.GetSummFinalCost(),objHomePage.GetFinalCost());
+    }
+
+    @Test (description = "Перейти в корзину")
+    public void CheckInCart() throws InterruptedException {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(7);
+        objHomePage.ClickCartIcon();
+        objCart = objHomePage.ClickInCart();
+        Thread.sleep(15000);
+        Assert.assertTrue(driver.findElement(objCart.CartTitel).isDisplayed());
+    }
+
+    @Test (description = "Перейти к оформлению")
+    public void CheckOformlenie() throws InterruptedException {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objLoginPage = new LoginPage(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(7);
+        objHomePage.ClickCartIcon();
+        objOformit = objHomePage.ClickOformitUnlogin();
+        objLoginPage.InputLogin();
+        objLoginPage.InputPassword();
+        objLoginPage.ClickLoginBtn();
+        Thread.sleep(15000);
+        Assert.assertTrue(driver.findElement(objOformit.OformitTitel).isDisplayed());
+    }
+
+    @Test (description = "Убавить количество для товара")
+    public void CheckNameCart(){
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.SelectCategory(objCatalog.NeedCategory);
+        objCatalog.AddItem(7);
+        objHomePage.ClickCartIcon();
+        Assert.assertEquals(objHomePage.GetCartName(),"Корзина");
+    }
+
+    @Test(description = "Удалить 1 товар и сложить сумму всех товаров и проверить общую сумму")
+    public void CheckFinalCostAfterRemove()  {
+        objHomePage = new HomePage(driver);
+        objCatalog = new Catalog(driver);
+        objCatalog.AddFirst8Item();
+        objHomePage.ClickCartIcon();
+        objHomePage.DeleteItem(2);
+        objHomePage.ClickCartIcon();
+        Assert.assertEquals(objHomePage.GetSummFinalCost(),objHomePage.GetFinalCost());
+    }
 
     @AfterMethod
     public void closebrowser(ITestResult testResult) throws IOException {
